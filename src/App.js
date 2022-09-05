c79cb5488a3c0bd07ed152751d4c9edb06e47e43
AkashAppanna <akashappanna77@gmail.com>
2022-09-05 13:49:15 +0530

    new feature

--- src/App.js
+++ src/App.js
@@ -8,12 +8,6 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        {/* <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="blogs" element={<Blogs />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<NoPage />} />
-        </Route> */}
         <Route path="/" element={<Login />} />
         <Route path="video" element={<Video />} />
         <Route path="date" element={<Date />} />
